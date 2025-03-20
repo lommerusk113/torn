42a024a08947a008f19c00bd279f3fae95486dce
joakimlandsverk <joakim.mork.landsverk@gmail.com>
2025-03-20 23:48:13 +0100

    #10 - Setup war trackers

---
+++ src/repository/ApiKeyRepository.ts
@@ -0,0 +1,55 @@
+import { createClient, SupabaseClient } from '@supabase/supabase-js';
+
+export class ApiKeyRepository {
+  private supabase: SupabaseClient;
+
+  constructor() {
+    this.supabase = createClient(
+      process.env.SUPABASE_URL || '',
+      process.env.SUPABASE_KEY || ''
+    );
+  }
+
+   /**
+ * Gets a random api key from the database
+ * @returns key as string
+ */
+  public async getRandomKey(): Promise<string> {
+    const { data, error } = await this.supabase
+        .from('api_keys')
+        .select('key')
+
+    if (error) {
+        throw new Error(`Failed to fetch API key: ${error.message}`);
+    }
+
+    if (!data) {
+        throw new Error('No API keys available');
+    }
+
+    return data[Math.floor(Math.random() * data.length)].key
+
+  }
+
+  /**
+ * Deletes an API key from the database
+ * @param key The ID of the API key to delete
+ * @returns True if deletion was successful, false otherwise
+ */
+  public async deleteKey(key: string): Promise<boolean> {
+    const { error } = await this.supabase
+    .from('api_keys')
+    .delete()
+    .eq('key', key);
+
+    if (error) {
+        console.error(`Failed to delete API key: ${error.message}`);
+        return false;
+    }
+
+    return true;
+
+  }
+}
+
+export default ApiKeyRepository
--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import express from "express";
 import cors from "cors";
 import routes from "./routes/index.js";
 import trackUsers from "./scripts/tracker.js"
+import {getEnemy, track} from "./scripts/factionTracker.js"
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -15,5 +16,10 @@ app.listen(PORT, async () => {
   console.log(`Server running on http://localhost:${PORT}`);
 
   trackUsers()
+  getEnemy()
+
   setInterval(trackUsers, 60000);
+  setInterval(getEnemy, 5 * 60 * 1000);
+  setInterval(track, 5 * 1000);
+
 });
